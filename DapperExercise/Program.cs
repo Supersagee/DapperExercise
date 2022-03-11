c2d499e6d33fc88b29d01216c9d65f25b0d6ee98
Supersagee <williamdsage@gmail.com>
2022-03-11 06:37:02 -0600

    ignore appsettings

---
+++ DapperExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
